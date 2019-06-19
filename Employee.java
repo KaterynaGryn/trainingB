ea56a81815d0288201d4bec392543429409353ce
Kateryna_Gryn <katya5gryn@gmail.com>
2019-06-19 21:56:00 +0300

    refactored Person class with Boris suggestions

---
+++ Employee.java
@@ -0,0 +1,10 @@
+public class Employee extends Person {
+private String employee;
+
+    public Employee(int id, String firstName, String lastName, int age) {
+        super(id, firstName, lastName, age);
+    }
+
+
+}
+

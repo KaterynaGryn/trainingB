e8c2ba86527eecd01c4e84ed87111bd606eb0728
Kateryna_Gryn <katya5gryn@gmail.com>
2019-06-19 22:06:30 +0300

    refactored Person class with Boris suggestions

--- Employee.java
+++ Employee.java
@@ -1,5 +1,17 @@
 public class Employee extends Person {
-private String employee;
+    private String employee;
+    private String role;
+    private Float salary;
+    private String company;
+
+    enum Company {
+        CIKLUM,
+        EPAM,
+        INFOPULSE,
+        WIX,
+        GLOBAL_LOGIC,
+        LUXOFT
+    }
 
     public Employee(int id, String firstName, String lastName, int age) {
         super(id, firstName, lastName, age);

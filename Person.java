a9ec1e1fc84bf6dd6791f42727edd79a6c2d715b
Kateryna_Gryn <katya5gryn@gmail.com>
2019-06-19 10:59:25 +0300

    refactored Person class with Boris suggestions

--- Person.java
+++ Person.java
@@ -1,48 +1,17 @@
 public class Person {
-    private String  surName;
-    private String name;
-    private int age;
-    private  String colorOfEyes;
     private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
 
 
-    public String getName(){ return name; }
-    public String getSurNameName(){ return surName; }
-    public int getAge(){ return age; }
-    public String getcolorOfEyes(){ return colorOfEyes; }
-
-
-
-    public Person(String name){
-
-        this.name=name;
-        this.surName=surName;
-        this.age=age;
-        this.colorOfEyes=colorOfEyes;
-    }
-
-    public String getSurName() {
-        return surName;
-    }
-
-    public void setSurName(String surName) {
-        this.surName = surName;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(int age) {
+    public Person(int id, String firstName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.age = age;
     }
 
-    public String getColorOfEyes() {
-        return colorOfEyes;
-    }
 
-    public void setColorOfEyes(String colorOfEyes) {
-        this.colorOfEyes = colorOfEyes;
-    }
 }
 

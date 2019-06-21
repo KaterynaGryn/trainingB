b48c4480550cc96e6dfabe80164c2e7be3d73952
Kateryna_Gryn <katya5gryn@gmail.com>
2019-06-21 18:30:13 +0300

    added getters and setters for Employee

--- Employee.java
+++ Employee.java
@@ -1,15 +1,89 @@
 public class Employee extends Person {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
     private String employee;
     private String role;
     private Float salary;
     private String company;
 
 
-
     public Employee(int id, String firstName, String lastName, int age, Enum role) {
         super(id, firstName, lastName, age);
     }
 
+    @Override
+    public int getId() {
+        return id;
+    }
 
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(String employee) {
+        this.employee = employee;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Float salary) {
+        this.salary = salary;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
 }
 
